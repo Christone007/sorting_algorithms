67c192d8573f8a638d6fb480d10acb9abbfd860e
Nwaburu Emeka Christian <exellentemy@gmail.com>
2023-03-12 03:36:56 +0000

    Fix linter errors

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,13 +1,19 @@
 #include "sort.h"
-
+/**
+ * selection_sort - Sorts items in an array using the select sort
+ * @array: The array of integers to be sorted
+ * @size: Size of the array
+ *
+ * Return: void
+ */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i, j, jMin;
 	int temp;
 
-	for (i = 0; i < size-1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		size_t jMin = i;
+		jMin = i;
 
 		for (j = i + 1; j < size; j++)
 		{
